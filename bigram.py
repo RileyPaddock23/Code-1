1540776d29d028c2c6a564c279d8be29988f9b4c
Riley Paddock <riley_paddock@ucsb.edu>
2024-01-11 12:28:38 -0800

    Small updates to disconnect sentence splits

--- bigram.py
+++ bigram.py
@@ -1,33 +1,43 @@
 import numpy as np
 
-f = open("./OscarWilde.txt", "r")
-substitutions = {k:k for k in "abcdefghijklmnopqrstuvwxyzã- "}
-substitutions["\n"] = " "
+# f = open("./OscarWilde.txt", "r")
+# substitutions = {k:k for k in "abcdefghijklmnopqrstuvwxyzã- "}
+# substitutions["\n"] = " "
+# substitutions["."] = " <E>"
+# adjusted_text = ""
+# for c in f.read().lower():
+#     try:
+#         adjusted_text += substitutions[c]
+#     except KeyError:
+#         adjusted_text += ""
 
-adjusted_text = ""
-for c in f.read().lower():
-    try:
-        adjusted_text += substitutions[c]
-    except KeyError:
-        adjusted_text += ""
+# o = open("./processedPODG.txt", "w")
+# o.write(adjusted_text)
 
-ordered_words = adjusted_text.split(" ")
+f = open("./processedPODG.txt","r")
+ordered_words = f.read().split(" ")
+ordered_words.remove("")
+ordered_words.remove("<E>")
 
 word_bank = list(set(ordered_words))
+
 stoi = {w:i for i,w in enumerate(word_bank)}
 itos = {stoi[key]:key for key in stoi}
-dim = len(set(ordered_words))
+dim = len(set(word_bank))
 probabs = np.zeros((dim, dim))#first index previous word, second next word occurance
 
 for i in range(1,len(ordered_words)):
-    probabs[stoi[ordered_words[i-1]]][stoi[ordered_words[i]]] += 1
+    if stoi[ordered_words[i-1]] != "<E>":
+        probabs[stoi[ordered_words[i-1]]][stoi[ordered_words[i]]] += 1
+    else:
+        print(i, end = "\r")
 
 for i in range(dim):
     s = sum(probabs[i])
     probabs[i] = [x/s for x in probabs[i]]
 
-np.save("bigram_arr.npy",probabs)
-# probabs = np.load("bigram_arr.npy")
+np.save("./bigram_arr.npy",probabs)
+probabs = np.load("./bigram_arr.npy")
 
 prev = np.random.choice(word_bank)
 for x in range(10):
